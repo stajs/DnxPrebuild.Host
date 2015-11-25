d5113b5ae6f81054e4427f61b50ab85e7ce7b470
stajs <shannonsnz@gmail.com>
2015-11-25 22:47:20 +1300

    Initial commit

---
+++ src/DnxPrebuild.Host.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnxPrebuild.Host.ConsoleApp
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("DnxPrebuild.Host.ConsoleApp");
+			Console.ReadKey();
+		}
+	}
+}
